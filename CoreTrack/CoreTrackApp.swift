c03200df4c5583aa8752b995bd91a4d73416b893
Luke <lukeforn@hotmail.com>
2025-07-25 17:23:18 +1000

    Initial Commit

---
+++ CoreTrack/CoreTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreTrackApp.swift
+//  CoreTrack
+//
+//  Created by Luke Fornieri on 25/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct CoreTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
